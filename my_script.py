f0e8586c68c5e01f0f52f7a3c04b293061a4bc97
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-16 16:49:06 -0400

    This code was supplied by the student

---
+++ my_script.py
@@ -0,0 +1,63 @@
+# from TH on 4/16
+
+from dotenv import load_dotenv
+import requests
+import json
+import os
+import tkinter
+
+load_dotenv()
+
+#Initialize GUI
+
+window = tkinter.Tk()
+
+search_label = tkinter.Label(text="Enter recipe name or key words here:")
+entry_value = tkinter.StringVar()
+search = tkinter.Entry(textvariable=entry_value)
+
+
+
+def search_button_click():
+    #code to run search
+    search_value = entry_value.get()
+    app_key = os.environ.get("my_app_key")
+    app_id = os.environ.get("my_app_id")
+    request_url = f"https://api.edamam.com/search?q={search_value}&app_id={app_id}&app_key={app_key}"
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+    recipe_list = []
+    recipes = parsed_response["hits"]
+    for recipe in recipes:
+        recipe_list.append(recipe["recipe"]["label"])
+
+    #add recipes to ListBox
+    recipe_select_label = tkinter.Label(text="Select a recipe you would like to add from the dropdown:")
+    recipe_select = tkinter.Listbox()
+    i = 1
+    for recipe in recipe_list:
+        recipe_select.insert(i, recipe)
+        i = i + 1
+    recipe_select_label.pack()
+    recipe_select.pack()
+
+search_button = tkinter.Button(text="Search", command=search_button_click)
+
+selected_recipes = []
+
+def add_recipe_click():
+    selected_recipes.append(recipe_select.get(recipe_select.curselection()))
+
+add_button = tkinter.Button(text="Add Recipe", command=add_recipe_click)
+
+print(selected_recipes)
+
+search_label.pack()
+search.pack()
+
+search_button.pack()
+add_button.pack()
+
+
+
+window.mainloop()
